890db73b75908dbb808003dc5e9c88eab33d12f5
bhavanat12 <bhavanat1298@gmail.com>
2023-11-01 09:14:20 -0400

    jc work msg

--- src/main/java/com/mindwell/springsql/LandingController.java
+++ src/main/java/com/mindwell/springsql/LandingController.java
@@ -8,6 +8,6 @@ public class LandingController {
 
 	@GetMapping("/")
 	public String hello(){
-		return "Hello World with latest changes!!!";
+		return "Hello World with latest changes forever!!!";
 	}
 }
